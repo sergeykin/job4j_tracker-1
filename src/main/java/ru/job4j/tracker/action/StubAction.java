06a0e43b94a2f95b1fc6954f27d6a4c1ea9b96f3
ShamRail <rail1999g102r@mail.ru>
2020-05-11 17:18:12 +0500

    9. Написать тесты на StartUI. [#785]

---
+++ src/main/java/ru/job4j/tracker/action/StubAction.java
@@ -0,0 +1,23 @@
+package ru.job4j.tracker.action;
+
+import ru.job4j.tracker.Tracker;
+import ru.job4j.tracker.input.Input;
+
+public class StubAction implements UserAction {
+    private boolean call = false;
+
+    @Override
+    public String name() {
+        return "Stub action";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        call = true;
+        return false;
+    }
+
+    public boolean isCall() {
+        return call;
+    }
+}
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,5 +1,21 @@
 package ru.job4j.tracker;
 
-public class StartUITest {
+import org.junit.Test;
+import ru.job4j.tracker.action.StubAction;
+import ru.job4j.tracker.action.UserAction;
+import ru.job4j.tracker.input.StubInput;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class StartUITest {
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
 }
