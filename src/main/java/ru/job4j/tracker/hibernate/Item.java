3802b862158766ff903f528d9db6402a25a1d234
Rail <rail1999g102r@mail.ru>
2020-07-18 16:20:19 +0500

    0. Конфигурация Hibernate [#320359]

---
+++ src/main/java/ru/job4j/tracker/hibernate/Item.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker.hibernate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s", id, name);
+    }
+}
